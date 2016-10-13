c759dcd4beafa44fcce90e63f3a369fba1dedda3
Brett Grigsby <brett@theflyfisher.com>
2016-10-13 16:35:25 -0600

    pulling employees from API and dropping into table

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -3,23 +3,52 @@ import { SessionService, User, Company } from '../session/session.service';
 import { EmployeesService } from './employees.service';
 import { Router } from '@angular/router';
 
+interface Employee {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    manager: string;
+    manager_id: number;
+    role: string;
+    pending_invitation: boolean;
+    managers: any;
+}
+
 @Component({
-  selector: 'app-employees',
-  templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css'],
-  providers: [EmployeesService] // declared here as it only services this component
+    selector: 'app-employees',
+    templateUrl: './employees.component.html',
+    styleUrls: ['./employees.component.css'],
+    providers: [EmployeesService] // declared here as it only services this component
 })
 export class EmployeesComponent implements OnInit {
-  user: User;
-  company: Company;
-  data: any;
+    user: User;
+    company: Company;
+    employees: Array<Employee>;
 
     constructor(private sessionService: SessionService, private router: Router, private employeesService: EmployeesService) {
+        if (!this.sessionService.isLoggedIn()) {
+            this.router.navigate(['/']);
+        }
 
+        this.user = this.sessionService.getUser();
+        this.company = this.sessionService.getCompany();
+
+        this.sessionService.companySwitch.subscribe(c => {
+            this.company = c;
+            this.index();
+        });
     }
 
     ngOnInit() {
+        this.index();
+    }
 
+    index() {
+        this.employeesService.index(this.company.id).subscribe(res => {
+            this.employees = res.json().company.employees;
+            console.log(this.employees);
+        });
     }
 
 }
