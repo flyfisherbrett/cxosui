11c68f56fe43ca375947113ae80b088f2043bb35
matt nery <mattbook@matts-MacBook-Pro-2.local>
2016-10-24 16:36:04 -0700

    WIP

--- src/app/shared/directives/qb-connect.directive.ts
+++ src/app/shared/directives/qb-connect.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, AfterViewInit } from '@angular/core';
-declare var $
+import { User } from '../user';
+declare var $, intuit;
 
 @Directive({
   selector: '[appqb-connect]'
@@ -9,6 +10,18 @@ export class QbConnectDirective {
 
   constructor() { }
   ngAfterViewInit() {
-    $('#modal-footer-content').append('<ipp:connectToIntuit></ipp:connectToIntuit>');
+    $('#sidebar').append('<ipp:connectToIntuit></ipp:connectToIntuit>');
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//js.appcenter.intuit.com/Content/IA/intuit.ipp.anywhere.js';
+
+    script.onload = function () {
+        intuit.ipp.anywhere.setup({
+        menuProxy: '/path/to/blue-dot',
+        grantUrl: ('api/oauth/authenticate'+ '?user_id=')
+      });
+    };
+
+    document.body.appendChild(script);
   }
 }
